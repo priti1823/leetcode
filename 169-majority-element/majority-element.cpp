d61dfe506f512debddbef7937fe18240d0d4f192
Priti Srivastava <pritisrivastava1823@gmail.com>
2026-08-29 14:08:51 +0530

    Time: 2236 ms (5.00%) | Memory: 28.3 MB (44.51%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        int b=nums.size()/2;
+        for(int i=0;i<nums.size();i++)
+        {int count=0;
+        for(int j=0;j<nums.size();j++)
+        {
+            if(nums[i]==nums[j])
+            {
+                count++;
+            }
+        }
+          if(count>b)
+          {
+            return nums[i];
+          }
+
+        }
+
+       return 0; 
+    }
+};
